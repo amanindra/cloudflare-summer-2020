f64d3b8fe6679b0a224ca890b8fdd7e8d56631dc
Manindra Kumar Anantaneni <a.manindra1995@gmail.com>
2020-04-14 21:18:04 -0500

    Extra Credit

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+/**
+ * @author Manindra Kumar Anantaneni
+ * Cloudflare Summer Internship 2020
+ */
+
+class LinkElementHandler {
+  element(element) {
+    element.setAttribute('href', 'https://amanindra.github.io');
+    element.setInnerContent("Manindra Kumar Anantaneni's Github Pages");
+  }
+}
+
+class TitleElementHandler {
+  element(element) {
+    element.before('<meta charset="utf-8">', { html: true });
+    element.prepend('Welcome! Visiting ');
+  }
+}
+
+class BodyElementHandler {
+  element(element) {
+    element.setInnerContent(
+      'Summer 2020 Internship Coding Challenge by CloudFlare, done by Manindra Kumar Anantaneni'
+    );
+  }
+}
+
 addEventListener('fetch', (event) => {
   event.respondWith(
     handleRequest(
@@ -11,7 +38,7 @@ addEventListener('fetch', (event) => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return fetch(request)
+  let response = await fetch(request)
     .then((response) => response.json())
     .then((responseData) => {
       let urls = [...responseData.variants];
@@ -21,4 +48,11 @@ async function handleRequest(request) {
       let i = Math.random() >= 0.5 ? 1 : 0;
       return fetch(urls[i]);
     });
+
+  let htmlRewrtiter = new HTMLRewriter()
+    .on('p#description', new BodyElementHandler())
+    .on('title', new TitleElementHandler())
+    .on('a#url', new LinkElementHandler());
+
+  return new Response(htmlRewrtiter.transform(response).body, response);
 }
